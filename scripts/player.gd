cfdb20c900ae30f9ca7ed7f33cd941fd39bf0024
Tarudahat <Taruzahat@gmail.com>
2025-04-05 17:31:43 +0200

    fix player, crab, level_1

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
+class_name Player
 
 @export var speed = 600
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var state = "Idle"
 var target = position
 var block_movement = false
 var can_throw = true
 var throw_target_position
 var items: Array[int] = [0,0,0,0,0,0]
+var rng = RandomNumberGenerator.new()
 var wood_minigame = preload("res://scenes/start_fire_minigame.tscn")
+var throwing_rock = preload("res://scenes/throw_stone.tscn")
 var wood_minigame_instance = null
 # shell_count
 # wood_count
@@ -27,38 +28,47 @@ func _process(delta):
 	$VBoxContainer/Label2.text = "wood: " + str(items[1])
 	$VBoxContainer/Label3.text = "sulfur: " + str(items[2])
 	$VBoxContainer/Label4.text = "coal: " + str(items[3])
+	$VBoxContainer/Label5.text = "snake rope: " + str(items[4])
+	$VBoxContainer/Label6.text = "stone: " + str(items[5])
 	
-	update_animation()
 
 func _input(event):
 	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
-	if not cutting_wood:
-		if event.is_action_pressed(&"left_click"):
-			target = get_global_mouse_position()
-			state = "Walk"
-		if event.is_action_pressed("make_coal") and items[1]:
-			cutting_wood = true
-			wood_minigame_instance = wood_minigame.instantiate()
-			wood_minigame_instance.position = self.position
-			wood_minigame_instance.player_node = self
+	if items[5] and Input.is_action_pressed("collect") and event.is_action_pressed(&"left_click"):
+		if can_throw:
+			items[5]-=1
+			var rock_instance = throwing_rock.instantiate()
+			throw_target_position = get_global_mouse_position()
+			rock_instance.movement_direction = position.direction_to(throw_target_position)
+			rock_instance.position = self.position + rock_instance.movement_direction*150
+			get_parent().add_child(rock_instance)
+			$throw_cooldown.start()
+			can_throw = false
 			
-			if items[1] > 5:
-				wood_minigame_instance.max_coal_reward = 5
-			else:
-				wood_minigame_instance.max_coal_reward = items[1]
+	elif event.is_action_pressed(&"left_click") and not block_movement:
+		target = get_global_mouse_position()
+	if event.is_action_pressed("make_coal") and items[1] and not block_movement:
+		block_movement = true
+		wood_minigame_instance = wood_minigame.instantiate()
+		wood_minigame_instance.position = self.position
+		wood_minigame_instance.player_node = self
 			
-			get_parent().add_child(wood_minigame_instance)
+		if items[1] > 5:
+			wood_minigame_instance.max_coal_reward = 5
+		else:
+			wood_minigame_instance.max_coal_reward = items[1]
+			
+		get_parent().add_child(wood_minigame_instance)
 
 func _physics_process(_delta):
 	velocity = position.direction_to(target) * speed
-	if cutting_wood:
+	if block_movement:
 		velocity = Vector2.ZERO
-	# look_at(target)
-	if position.distance_to(target) > 10 and not cutting_wood:
+	if position.distance_to(target) > 10 and not block_movement:
 		move_and_slide()
 
-func update_animation() -> void:
-	animation_player.play(state + "_" + animation_direction() )
+#func update_animation() -> void:
+#	animation_player.play(state + "_" + animation_direction() )
 
 func animation_direction() -> String:
 	if target.x > 0:
@@ -72,3 +82,7 @@ func animation_direction() -> String:
 		else:
 			return "left"	
 	
+
+
+func _on_throw_cooldown_timeout() -> void:
+	can_throw = true
