1d791cd99f5d948062473e7494a23f5b5fbdaa32
Tarudahat <Taruzahat@gmail.com>
2025-04-06 01:55:39 +0200

    add chest

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -4,6 +4,7 @@ const time_s_per_level: Array[int] = [5*60,5*60,5*60,1*60]
 var current_level: int = 0
 var items: Array[int] = [0,0,0,0,0,0]
 var time_left = 0 # + per level time
+var treasures_found = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
---
+++ scripts/chest.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+var treasure_counted: bool = false
+
+var player_in_range: bool = false
+var player_node: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$E_indicator.visible = false
+	
+	if player_in_range:
+		if $AnimatedSprite2D.frame < 3:
+			$E_indicator.visible = true
+		else:
+			if not treasure_counted:
+				Globals.treasures_found += 1
+				treasure_counted = true
+	
+	if Input.is_action_just_pressed("collect"):
+		if player_in_range and player_node and $AnimatedSprite2D.frame < 3:
+			$AnimatedSprite2D.frame += 1
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_node = body
+		player_in_range = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_range = false
