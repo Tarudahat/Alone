595a4074bcb4ffcb1d858f9cef73ec185e5e42c5
Tarudahat <Taruzahat@gmail.com>
2025-04-06 13:04:40 +0200

    make crab nerd again

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const time_s_per_level: Array[int] = [5*60,5*60,5*60,1*60]
+const time_s_per_level: Array[int] = [5*60,5*60,1*60]
 var current_level: int = 0
 var items: Array[int] = [0,0,0,0,0,0]
 var time_left = 0 # + per level time
